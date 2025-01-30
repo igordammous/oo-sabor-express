5ed744e0c86cdc8fc7d93131a457c239e689b04f
Igor Dammous <igordammous@gmail.com>
2025-01-30 14:47:52 -0300

    docs: atualização das classes do restaurante e exercicios aula 1

---
+++ exercicios/exercicios-1-oo.py
@@ -0,0 +1,11 @@
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = float
+
+musica_starway_to_heaven = Musica()
+musica_starway_to_heaven.nome = 'Starway to Heaven'
+musica_starway_to_heaven.artista = 'Led Zepplin'
+musica_starway_to_heaven.duracao = 8.03
+
+print(vars(musica_starway_to_heaven))
